42bfedcf577553547f1540bfefab532c29725202
Connum <connum@gmail.com>
2019-03-16 15:47:50 +0100

    update all dependencies to latest versions and make some linting adaptions to prevent errors. set package version to 1.0.4

--- test/index.js
+++ test/index.js
@@ -3,18 +3,18 @@ import pinyinSeparate from '../src';
 
 describe('Default splitting tests', () => {
   it('should split jiānrěnbùbá', () => {
-    const expectedVal = ['jiān', 'rěn', 'bù' , 'bá']
+    const expectedVal = ['jiān', 'rěn', 'bù', 'bá'];
     assert.deepEqual(pinyinSeparate('jiānrěnbùbá'), expectedVal);
   });
-  
+
   it('should split nĭhăoma using breve instead of háček for 3rd tone', () => {
-    const expectedVal = ['nĭ', 'hăo', 'ma' ]
+    const expectedVal = ['nĭ', 'hăo', 'ma'];
     assert.deepEqual(pinyinSeparate('nĭhăoma'), expectedVal);
   });
 
   it('should split jiān rěn bù bá with non-breaking spaces', () => {
-    const expectedVal = ['jiān', 'rěn', 'bù' , 'bá']
-    const nbsp = String.fromCharCode(160)
+    const expectedVal = ['jiān', 'rěn', 'bù', 'bá'];
+    const nbsp = String.fromCharCode(160);
     assert.deepEqual(pinyinSeparate(`jiān${nbsp}rěn${nbsp}bù${nbsp}bá`, {
       byNbsp: true
     }), expectedVal);
